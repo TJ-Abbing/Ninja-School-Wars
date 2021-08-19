bb44c3c0800106c724fff084c3df40f619cac3ed
TJ <71647978+TJ-CMGT@users.noreply.github.com>
2021-08-19 15:58:13 +0200

    Player fully animated

--- dev/game.ts
+++ dev/game.ts
@@ -13,8 +13,8 @@ class playerClass {
     }
     x = 200;
     y = 200;
-    width = 200;
-    height = 200;
+    width = 143;
+    height = 417;
     frameX = 0;
     frameY = 0;
     speed = 9;
@@ -23,8 +23,8 @@ class playerClass {
 
 let player = new playerClass; 
 
-const playerSprite = new Image();
-playerSprite.src = "docs/images/ninja.png";
+let playerSprite = new Image();
+playerSprite.src = "docs/images/ninja_all.png";
 
 const background = new Image();
 background.src = "docs/images/background.png";
@@ -36,7 +36,7 @@ function drawSprite(img: CanvasImageSource, sX: number, sY: number, sW: number, 
 function animate(){
     ctx?.clearRect(0, 0, canvas.width, canvas.height);
     ctx?.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite(playerSprite, 0, 0, player.width, player.height, player.x, player.y, player.width / 2, player.height / 2);
+    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width / 2, player.height / 2);
     movePlayer();
     requestAnimationFrame(animate);
 }
@@ -52,17 +52,22 @@ window.addEventListener("keyup", function(e){
 });
 
 function movePlayer(){
-    if (keys[38] && player.y > 0){
+    if (keys[38] && player.y > 10){
         player.y -= player.speed;
-    }
-    if (keys[37] && player.x > 0){
+        player.frameX = 0
+        }
+    if (keys[37] && player.x > 10){
         player.x -= player.speed;
-    }
-    if (keys[40] && player.y <canvas.height - player.height + 90){
+        player.frameX = 2
+        }
+    if (keys[40] && player.y <canvas.height - player.height + 320){
         player.y += player.speed;
+        player.frameX = 0
     }
-    if (keys[39] && player.x <canvas.width - player.width + 100){
+    if (keys[39] && player.x <canvas.width - player.width + 70){
         player.x += player.speed;
+        player.frameX = 1
     }
 
 }
+
--- docs/js/game.js
+++ docs/js/game.js
@@ -10,8 +10,8 @@ var playerClass = /** @class */ (function () {
     function playerClass() {
         this.x = 200;
         this.y = 200;
-        this.width = 200;
-        this.height = 200;
+        this.width = 143;
+        this.height = 417;
         this.frameX = 0;
         this.frameY = 0;
         this.speed = 9;
@@ -22,7 +22,7 @@ var playerClass = /** @class */ (function () {
 }());
 var player = new playerClass;
 var playerSprite = new Image();
-playerSprite.src = "docs/images/ninja.png";
+playerSprite.src = "docs/images/ninja_all.png";
 var background = new Image();
 background.src = "docs/images/background.png";
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
@@ -31,7 +31,7 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
 function animate() {
     ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite(playerSprite, 0, 0, player.width, player.height, player.x, player.y, player.width / 2, player.height / 2);
+    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width / 2, player.height / 2);
     movePlayer();
     requestAnimationFrame(animate);
 }
@@ -44,16 +44,20 @@ window.addEventListener("keyup", function (e) {
     delete keys[e.keyCode];
 });
 function movePlayer() {
-    if (keys[38] && player.y > 0) {
+    if (keys[38] && player.y > 10) {
         player.y -= player.speed;
+        player.frameX = 0;
     }
-    if (keys[37] && player.x > 0) {
+    if (keys[37] && player.x > 10) {
         player.x -= player.speed;
+        player.frameX = 2;
     }
-    if (keys[40] && player.y < canvas.height - player.height + 90) {
+    if (keys[40] && player.y < canvas.height - player.height + 320) {
         player.y += player.speed;
+        player.frameX = 0;
     }
-    if (keys[39] && player.x < canvas.width - player.width + 100) {
+    if (keys[39] && player.x < canvas.width - player.width + 70) {
         player.x += player.speed;
+        player.frameX = 1;
     }
 }
