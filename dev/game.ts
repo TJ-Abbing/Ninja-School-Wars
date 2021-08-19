14687e5e264a6776ca377a823e0fdf68e3ae33a0
TJ <71647978+TJ-CMGT@users.noreply.github.com>
2021-08-19 14:05:28 +0200

    Added sprites and moveable player

--- dev/game.ts
+++ dev/game.ts
@@ -4,17 +4,17 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 500;
 
-const keys = [];
+const keys: any[] = [];
 
 // Player classes
 class playerClass {
     constructor(){
         console.log('Player 1 created!')
     }
-    x = 0;
-    y = 0;
-    width = 100;
-    height = 100;
+    x = 200;
+    y = 200;
+    width = 200;
+    height = 200;
     frameX = 0;
     frameY = 0;
     speed = 9;
@@ -24,7 +24,7 @@ class playerClass {
 let player = new playerClass; 
 
 const playerSprite = new Image();
-playerSprite.src = "doc/image/ninja.png";
+playerSprite.src = "docs/images/ninja.png";
 
 const background = new Image();
 background.src = "docs/images/background.png";
@@ -33,11 +33,36 @@ function drawSprite(img: CanvasImageSource, sX: number, sY: number, sW: number, 
     ctx?.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
 
-function animate() {
+function animate(){
     ctx?.clearRect(0, 0, canvas.width, canvas.height);
     ctx?.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite(playerSprite, 0, 0, player.width, player.height, 0, 0, player.width, player.height);
+    drawSprite(playerSprite, 0, 0, player.width, player.height, player.x, player.y, player.width / 2, player.height / 2);
+    movePlayer();
     requestAnimationFrame(animate);
 }
 animate(); 
 
+window.addEventListener("keydown", function(e){
+    keys[e.keyCode] = true;
+    console.log(keys);
+});
+
+window.addEventListener("keyup", function(e){
+    delete keys[e.keyCode];
+});
+
+function movePlayer(){
+    if (keys[38] && player.y > 0){
+        player.y -= player.speed;
+    }
+    if (keys[37] && player.x > 0){
+        player.x -= player.speed;
+    }
+    if (keys[40] && player.y <canvas.height - player.height + 90){
+        player.y += player.speed;
+    }
+    if (keys[39] && player.x <canvas.width - player.width + 100){
+        player.x += player.speed;
+    }
+
+}
--- docs/js/game.js
+++ docs/js/game.js
@@ -8,10 +8,10 @@ var keys = [];
 // Player classes
 var playerClass = /** @class */ (function () {
     function playerClass() {
-        this.x = 0;
-        this.y = 0;
-        this.width = 100;
-        this.height = 100;
+        this.x = 200;
+        this.y = 200;
+        this.width = 200;
+        this.height = 200;
         this.frameX = 0;
         this.frameY = 0;
         this.speed = 9;
@@ -22,7 +22,7 @@ var playerClass = /** @class */ (function () {
 }());
 var player = new playerClass;
 var playerSprite = new Image();
-playerSprite.src = "doc/image/ninja.png";
+playerSprite.src = "docs/images/ninja.png";
 var background = new Image();
 background.src = "docs/images/background.png";
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
@@ -31,7 +31,29 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
 function animate() {
     ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite(playerSprite, 0, 0, player.width, player.height, 0, 0, player.width, player.height);
+    drawSprite(playerSprite, 0, 0, player.width, player.height, player.x, player.y, player.width / 2, player.height / 2);
+    movePlayer();
     requestAnimationFrame(animate);
 }
 animate();
+window.addEventListener("keydown", function (e) {
+    keys[e.keyCode] = true;
+    console.log(keys);
+});
+window.addEventListener("keyup", function (e) {
+    delete keys[e.keyCode];
+});
+function movePlayer() {
+    if (keys[38] && player.y > 0) {
+        player.y -= player.speed;
+    }
+    if (keys[37] && player.x > 0) {
+        player.x -= player.speed;
+    }
+    if (keys[40] && player.y < canvas.height - player.height + 90) {
+        player.y += player.speed;
+    }
+    if (keys[39] && player.x < canvas.width - player.width + 100) {
+        player.x += player.speed;
+    }
+}
