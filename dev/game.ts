f0d2f1150ac9ba08140ae8babf78350116805c18
TJ <71647978+TJ-CMGT@users.noreply.github.com>
2021-08-20 16:50:28 +0200

    Added shuriken (not functional as weapon yet)

--- dev/game.ts
+++ dev/game.ts
@@ -1,18 +1,18 @@
+//Setup
 console.log('JS is running!')
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 500;
-
 const keys: any[] = [];
 
 // player1 classes
-class player1Class {
+class player1Class{
     constructor(){
         console.log('player1 1 created!')
     }
-    x = 300;
-    y = 400;
+    x = 50;
+    y = 300;
     width = 143;
     height = 417;
     frameX = 0;
@@ -25,12 +25,12 @@ class player1Class {
 }
 
 // player2 classes
-class player2Class {
+class player2Class{
     constructor(){
         console.log('player1 2 created!')
     }
-    x = 600;
-    y = 400;
+    x = 650;
+    y = 300;
     width = 143;
     height = 417;
     frameX = 0;
@@ -42,58 +42,81 @@ class player2Class {
     jumping = false;
 }
 
+// Shuriken class
+class shurikenClass{
+    constructor(){
+        console.log('shuriken created!')
+    }
+    x = 400;
+    y = 300;
+    width = 33;
+    height = 33;
+    frameX = 0;
+    frameY = 0;
+    speed = 9;
+    right = 9;
+}
+// Creating players + shuriken
 let player1 = new player1Class; 
 let player2 = new player2Class; 
-
+let shuriken = new shurikenClass;
+function moveShuriken(){
+    shuriken.x += shuriken.right
+}
+// Adding sprites 
 let player1Sprite = new Image();
 player1Sprite.src = "docs/images/ninja_black.png";
 let player2Sprite = new Image();
 player2Sprite.src = "docs/images/ninja_white.png";
+let shurikenSprite = new Image;
+shurikenSprite.src = "docs/images/shuriken.png";
 
+// Adding background
 const background = new Image();
 background.src = "docs/images/background.png";
 
+// Drawing sprites function for players
 function drawSprite1(img: CanvasImageSource, sX: number, sY: number, sW: number, sH: number, dX: number, dY: number, dW: number, dH: number) {
     ctx?.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
-
 function drawSprite2(img: CanvasImageSource, sX: number, sY: number, sW: number, sH: number, dX: number, dY: number, dW: number, dH: number) {
     ctx?.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
-
+function drawSprite3(img: CanvasImageSource, sX: number, sY: number, sW: number, sH: number, dX: number, dY: number, dW: number, dH: number) {
+    ctx?.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+// Animates background, players and shurikens
 function animate(){
     ctx?.clearRect(0, 0, canvas.width, canvas.height);
     ctx?.drawImage(background, 0, 0, canvas.width, canvas.height);
     drawSprite1(player1Sprite, player1.width * player1.frameX, player1.height * player1.frameY, player1.width, player1.height, player1.x, player1.y, player1.width / 2, player1.height / 2);
     drawSprite2(player2Sprite, player2.width * player2.frameX, player2.height * player2.frameY, player2.width, player2.height, player2.x, player2.y, player2.width / 2, player2.height / 2);
+    drawSprite3(shurikenSprite, shuriken.width * shuriken.frameX, shuriken.height * shuriken.frameY, shuriken.width, shuriken.height, shuriken.x, shuriken.y, shuriken.width / 2, shuriken.height / 2);
     moveplayer1();
     moveplayer2();
+    moveShuriken();
     requestAnimationFrame(animate);
 }
 animate(); 
 
+
+// Adding keys
 window.addEventListener("keydown", function(e){
     keys[e.keyCode] = true;
 });
-
 window.addEventListener("keyup", function(e){
     delete keys[e.keyCode];
 });
 
+// Adding jump for players
 function jump1(){
     player1.y -= 30;
 }
-
 function jump2(){
     player2.y -= 30;
 }
 
-var delayInMilliseconds = 1000; //1 second
-
-setTimeout(function() {
-  //your code to be executed after 1 second
-}, delayInMilliseconds);
-
+// Adding player controls
 function moveplayer1(){
     if (player1.y <canvas.height - player1.height + 320){
         player1.y += player1.gravity
@@ -101,7 +124,6 @@ function moveplayer1(){
 
     if (keys[87] && player1.y > 30){
         console.log("player 1 up")
-        // player1.y -= player1.speed;
         player1.frameX = 0
         jump1();
         }
@@ -121,7 +143,6 @@ function moveplayer1(){
         player1.frameX = 1
     }
 }
-
 function moveplayer2(){
     if (player2.y <canvas.height - player2.height + 320){
         player2.y += player2.gravity
@@ -129,7 +150,6 @@ function moveplayer2(){
 
     if (keys[38] && player2.y > 30){
         console.log("player 2 up")
-        // player2.y -= player2.speed;
         player2.frameX = 0
         jump2();
         }
@@ -149,11 +169,13 @@ function moveplayer2(){
         player2.frameX = 1
     }
 }
-
-let healthP1;
-healthP1 = document.getElementById('p1-health');
-healthP1.style.width = player1.health + "%";
-
-let healthP2;
-healthP2 = document.getElementById('p2-health');
-healthP2.style.width = player2.health + "%";
+// Players healthbar
+var delayInMilliseconds = 200; // 0.1 second delay
+setTimeout(function() {
+  let healthP1;
+  healthP1 = document.getElementById('p1-health');
+  healthP1.style.width = player1.health + "%";
+  let healthP2;
+  healthP2 = document.getElementById('p2-health');
+  healthP2.style.width = player2.health + "%";
+}, delayInMilliseconds);
--- docs/js/game.js
+++ docs/js/game.js
@@ -1,4 +1,5 @@
 "use strict";
+//Setup
 console.log('JS is running!');
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
@@ -8,8 +9,8 @@ var keys = [];
 // player1 classes
 var player1Class = /** @class */ (function () {
     function player1Class() {
-        this.x = 300;
-        this.y = 400;
+        this.x = 50;
+        this.y = 300;
         this.width = 143;
         this.height = 417;
         this.frameX = 0;
@@ -26,8 +27,8 @@ var player1Class = /** @class */ (function () {
 // player2 classes
 var player2Class = /** @class */ (function () {
     function player2Class() {
-        this.x = 600;
-        this.y = 400;
+        this.x = 650;
+        this.y = 300;
         this.width = 143;
         this.height = 417;
         this.frameX = 0;
@@ -41,53 +42,82 @@ var player2Class = /** @class */ (function () {
     }
     return player2Class;
 }());
+// Shuriken class
+var shurikenClass = /** @class */ (function () {
+    function shurikenClass() {
+        this.x = 400;
+        this.y = 300;
+        this.width = 33;
+        this.height = 33;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.speed = 9;
+        this.right = 9;
+        console.log('shuriken created!');
+    }
+    return shurikenClass;
+}());
+// Creating players + shuriken
 var player1 = new player1Class;
 var player2 = new player2Class;
+var shuriken = new shurikenClass;
+function moveShuriken() {
+    shuriken.x += shuriken.right;
+}
+// Adding sprites 
 var player1Sprite = new Image();
 player1Sprite.src = "docs/images/ninja_black.png";
 var player2Sprite = new Image();
 player2Sprite.src = "docs/images/ninja_white.png";
+var shurikenSprite = new Image;
+shurikenSprite.src = "docs/images/shuriken.png";
+// Adding background
 var background = new Image();
 background.src = "docs/images/background.png";
+// Drawing sprites function for players
 function drawSprite1(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
 function drawSprite2(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
+function drawSprite3(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+    ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+// Animates background, players and shurikens
 function animate() {
     ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     drawSprite1(player1Sprite, player1.width * player1.frameX, player1.height * player1.frameY, player1.width, player1.height, player1.x, player1.y, player1.width / 2, player1.height / 2);
     drawSprite2(player2Sprite, player2.width * player2.frameX, player2.height * player2.frameY, player2.width, player2.height, player2.x, player2.y, player2.width / 2, player2.height / 2);
+    drawSprite3(shurikenSprite, shuriken.width * shuriken.frameX, shuriken.height * shuriken.frameY, shuriken.width, shuriken.height, shuriken.x, shuriken.y, shuriken.width / 2, shuriken.height / 2);
     moveplayer1();
     moveplayer2();
+    moveShuriken();
     requestAnimationFrame(animate);
 }
 animate();
+// Adding keys
 window.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true;
 });
 window.addEventListener("keyup", function (e) {
     delete keys[e.keyCode];
 });
+// Adding jump for players
 function jump1() {
     player1.y -= 30;
 }
 function jump2() {
     player2.y -= 30;
 }
-var delayInMilliseconds = 1000; //1 second
-setTimeout(function () {
-    //your code to be executed after 1 second
-}, delayInMilliseconds);
+// Adding player controls
 function moveplayer1() {
     if (player1.y < canvas.height - player1.height + 320) {
         player1.y += player1.gravity;
     }
     if (keys[87] && player1.y > 30) {
         console.log("player 1 up");
-        // player1.y -= player1.speed;
         player1.frameX = 0;
         jump1();
     }
@@ -113,7 +143,6 @@ function moveplayer2() {
     }
     if (keys[38] && player2.y > 30) {
         console.log("player 2 up");
-        // player2.y -= player2.speed;
         player2.frameX = 0;
         jump2();
     }
@@ -133,9 +162,13 @@ function moveplayer2() {
         player2.frameX = 1;
     }
 }
-var healthP1;
-healthP1 = document.getElementById('p1-health');
-healthP1.style.width = player1.health + "%";
-var healthP2;
-healthP2 = document.getElementById('p2-health');
-healthP2.style.width = player2.health + "%";
+// Players healthbar
+var delayInMilliseconds = 200; // 0.1 second delay
+setTimeout(function () {
+    var healthP1;
+    healthP1 = document.getElementById('p1-health');
+    healthP1.style.width = player1.health + "%";
+    var healthP2;
+    healthP2 = document.getElementById('p2-health');
+    healthP2.style.width = player2.health + "%";
+}, delayInMilliseconds);
