1c677a0ca6430d4894fcffb4aea89b0ab10ff0c9
TJ <71647978+TJ-CMGT@users.noreply.github.com>
2021-08-21 20:53:34 +0200

    Fully functional (but only score based atm)

--- dev/game.ts
+++ dev/game.ts
@@ -1,75 +1,140 @@
 //Setup
+function game(){
+    
+}
 console.log('JS is running!')
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 500;
+let gameFrame = 0;
+let scoreP1 = 0;
+let scoreP2 = 0
+let win = false;
 const keys: any[] = [];
 
 // player1 classes
-class player1Class{
+class playersClass{
     constructor(){
-        console.log('player1 1 created!')
+        console.log('player created!')
     }
-    x = 50;
+    x = 0;
     y = 300;
     width = 143;
-    height = 417;
+    height = 405;
     frameX = 0;
     frameY = 0;
     speed = 9;
-    gravity = 5;
+    gravity = 9;
     health = 100;
-    moving = false;
-    jumping = false;
+    radius = 35;
 }
 
-// player2 classes
-class player2Class{
-    constructor(){
-        console.log('player1 2 created!')
+// White shurikens
+const shurikenArrayWhite: any = [];
+class shurikenWhite {
+    x = canvas.width + Math.random() * canvas.width;
+    y = Math.random() * canvas.height;
+    radius = 10;
+    speed = Math.random() * 5 + 10;
+    counted = false;
+    distance = 0;
+
+    update(){
+        this.x -= this.speed; 
+        const dx = this.x - player1.x;
+        const dy = this.y - player1.y;
+        this.distance = Math.sqrt(dx*dx + dy*dy);
     }
-    x = 650;
-    y = 300;
-    width = 143;
-    height = 417;
-    frameX = 0;
-    frameY = 0;
-    speed = 9;
-    gravity = 5;
-    health = 100;
-    moving = false;
-    jumping = false;
+    draw(){
+        ctx?.fillStyle = "white";
+        ctx?.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx?.fill();
+        ctx?.closePath();
+        ctx?.stroke();
+    }
+}
+function shurikenHandlerWhite(){
+    if (gameFrame % 10 == 0){
+        shurikenArrayWhite.push(new shurikenWhite());
+    } 
+    for (let i = 0; i < shurikenArrayWhite.length; i++){
+        shurikenArrayWhite[i].update();
+        shurikenArrayWhite[i].draw();
+
+    }
+    for (let i = 0; i < shurikenArrayWhite.length; i++){
+        if (shurikenArrayWhite[i].x < 0 ){
+            shurikenArrayWhite.splice(i, 1);
+        }
+        if (shurikenArrayWhite[i].distance < shurikenArrayWhite[i].radius + player1.radius){
+            scoreP1++;
+            if (!shurikenArrayWhite[i].counted){
+                shurikenArrayWhite[i].counted = true;
+                shurikenArrayWhite.splice(i, 1);
+            }
+        }
+    }
 }
 
-// Shuriken class
-class shurikenClass{
-    constructor(){
-        console.log('shuriken created!')
+// Black shurikens
+const shurikenArrayBlack: any = [];
+class shurikenBlack {
+    x = canvas.width + Math.random() * canvas.width;
+    y = Math.random() * canvas.height;
+    radius = 10;
+    speed = Math.random() * 5 + 10;
+    counted = false;
+    distance = 0;
+
+    update(){
+        this.x -= this.speed; 
+        const dx = this.x - player2.x;
+        const dy = this.y - player2.y;
+        this.distance = Math.sqrt(dx*dx + dy*dy);
     }
-    x = 400;
-    y = 300;
-    width = 33;
-    height = 33;
-    frameX = 0;
-    frameY = 0;
-    speed = 9;
-    right = 9;
+    draw(){
+        ctx?.fillStyle = "black";
+        ctx?.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx?.fill();
+        ctx?.closePath();
+        ctx?.stroke();
+    }
 }
-// Creating players + shuriken
-let player1 = new player1Class; 
-let player2 = new player2Class; 
-let shuriken = new shurikenClass;
-function moveShuriken(){
-    shuriken.x += shuriken.right
+function shurikenHandlerBlack(){
+    if (gameFrame % 10 == 0){
+        shurikenArrayBlack.push(new shurikenBlack());
+    } 
+    for (let i = 0; i < shurikenArrayBlack.length; i++){
+        shurikenArrayBlack[i].update();
+        shurikenArrayBlack[i].draw();
+
+    }
+    for (let i = 0; i < shurikenArrayBlack.length; i++){
+        if (shurikenArrayBlack[i].x < 0 ){
+            shurikenArrayBlack.splice(i, 1);
+        }
+        if (shurikenArrayBlack[i].distance < shurikenArrayBlack[i].radius + player2.radius){
+            scoreP2++;
+            if (!shurikenArrayBlack[i].counted){
+                shurikenArrayBlack[i].counted = true;
+                shurikenArrayBlack.splice(i, 1);
+            }
+        }
+    }
 }
+let player1 = new playersClass; 
+let player2 = new playersClass; 
+
 // Adding sprites 
 let player1Sprite = new Image();
 player1Sprite.src = "docs/images/ninja_black.png";
 let player2Sprite = new Image();
 player2Sprite.src = "docs/images/ninja_white.png";
-let shurikenSprite = new Image;
-shurikenSprite.src = "docs/images/shuriken.png";
+// let shuriken1Sprite = new Image;
+// shuriken1Sprite.src = "docs/images/shuriken1.png";
 
 // Adding background
 const background = new Image();
@@ -82,22 +147,31 @@ function drawSprite1(img: CanvasImageSource, sX: number, sY: number, sW: number,
 function drawSprite2(img: CanvasImageSource, sX: number, sY: number, sW: number, sH: number, dX: number, dY: number, dW: number, dH: number) {
     ctx?.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
-function drawSprite3(img: CanvasImageSource, sX: number, sY: number, sW: number, sH: number, dX: number, dY: number, dW: number, dH: number) {
-    ctx?.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
-}
-// Animates background, players and shurikens
+
 function animate(){
-    ctx?.clearRect(0, 0, canvas.width, canvas.height);
-    ctx?.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite1(player1Sprite, player1.width * player1.frameX, player1.height * player1.frameY, player1.width, player1.height, player1.x, player1.y, player1.width / 2, player1.height / 2);
-    drawSprite2(player2Sprite, player2.width * player2.frameX, player2.height * player2.frameY, player2.width, player2.height, player2.x, player2.y, player2.width / 2, player2.height / 2);
-    drawSprite3(shurikenSprite, shuriken.width * shuriken.frameX, shuriken.height * shuriken.frameY, shuriken.width, shuriken.height, shuriken.x, shuriken.y, shuriken.width / 2, shuriken.height / 2);
-    moveplayer1();
-    moveplayer2();
-    moveShuriken();
-    requestAnimationFrame(animate);
+    if (!win){
+        ctx?.clearRect(0, 0, canvas.width, canvas.height);
+        ctx?.drawImage(background, 0, 0, canvas.width, canvas.height);
+        ctx?.fillText(`points:` + scoreP1, 10, 20);
+        ctx?.fillText(`points:` + scoreP2, 650, 20);
+        ctx?.font = '20px ariel';
+        drawSprite1(player1Sprite, player1.width * player1.frameX, player1.height * player1.frameY, player1.width, player1.height, player1.x, player1.y, player1.width / 2, player1.height / 2);
+        drawSprite2(player2Sprite, player2.width * player2.frameX, player2.height * player2.frameY, player2.width, player2.height, player2.x, player2.y, player2.width / 2, player2.height / 2);
+        moveplayer1();
+        moveplayer2();
+        requestAnimationFrame(animate);
+        shurikenHandlerWhite();
+        shurikenHandlerBlack();
+        gameFrame++;
+        checkWin()
+    }
 }
 animate(); 
+function setPlayerPositions(){
+    player1.x = 50;
+    player2.x = 650;
+}
+setPlayerPositions();
 
 
 // Adding keys
@@ -121,24 +195,19 @@ function moveplayer1(){
     if (player1.y <canvas.height - player1.height + 320){
         player1.y += player1.gravity
     }
-
     if (keys[87] && player1.y > 30){
-        console.log("player 1 up")
         player1.frameX = 0
         jump1();
         }
     if (keys[83] && player1.y <canvas.height - player1.height + 320){
-        console.log("player 1 down")
         player1.y += player1.speed;
         player1.frameX = 0
     }
     if (keys[65] && player1.x > 20){
-        console.log("player 1 left")
         player1.x -= player1.speed;
         player1.frameX = 2
-        }
+    }
     if (keys[68] && player1.x <canvas.width - player1.width + 60){
-        console.log("player 1 right")
         player1.x += player1.speed;
         player1.frameX = 1
     }
@@ -149,33 +218,36 @@ function moveplayer2(){
     }
 
     if (keys[38] && player2.y > 30){
-        console.log("player 2 up")
         player2.frameX = 0
         jump2();
         }
     if (keys[40] && player2.y <canvas.height - player2.height + 320){
-        console.log("player 2 down")
         player2.y += player2.speed;
         player2.frameX = 0
     }
     if (keys[37] && player2.x > 20){
-        console.log("player 2 left")
         player2.x -= player2.speed;
         player2.frameX = 2
         }
     if (keys[39] && player2.x <canvas.width - player2.width + 60){
-        console.log("player 2 right")
         player2.x += player2.speed;
         player2.frameX = 1
     }
 }
-// Players healthbar
-var delayInMilliseconds = 200; // 0.1 second delay
-setTimeout(function() {
-  let healthP1;
-  healthP1 = document.getElementById('p1-health');
-  healthP1.style.width = player1.health + "%";
-  let healthP2;
-  healthP2 = document.getElementById('p2-health');
-  healthP2.style.width = player2.health + "%";
-}, delayInMilliseconds);
+
+var delayInMilliseconds = 100; // Adds a 0.1 second delay untill game stops after a player wins
+
+function checkWin(){
+    if (scoreP1 >= 10){
+        ctx?.fillText(`Black ninja wins!`,10, 50);
+        setTimeout(function() {
+            win = true;
+          }, delayInMilliseconds);
+    }
+    if (scoreP2 >= 10){
+        ctx?.fillText(`White ninja wins!`,650, 50);
+        setTimeout(function() {
+            win = true;
+          }, delayInMilliseconds);
+    }
+}
--- docs/js/game.js
+++ docs/js/game.js
@@ -1,76 +1,139 @@
 "use strict";
 //Setup
+function game() {
+}
 console.log('JS is running!');
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 500;
+var gameFrame = 0;
+var scoreP1 = 0;
+var scoreP2 = 0;
+var win = false;
 var keys = [];
 // player1 classes
-var player1Class = /** @class */ (function () {
-    function player1Class() {
-        this.x = 50;
+var playersClass = /** @class */ (function () {
+    function playersClass() {
+        this.x = 0;
         this.y = 300;
         this.width = 143;
-        this.height = 417;
+        this.height = 405;
         this.frameX = 0;
         this.frameY = 0;
         this.speed = 9;
-        this.gravity = 5;
+        this.gravity = 9;
         this.health = 100;
-        this.moving = false;
-        this.jumping = false;
-        console.log('player1 1 created!');
+        this.radius = 35;
+        console.log('player created!');
     }
-    return player1Class;
+    return playersClass;
 }());
-// player2 classes
-var player2Class = /** @class */ (function () {
-    function player2Class() {
-        this.x = 650;
-        this.y = 300;
-        this.width = 143;
-        this.height = 417;
-        this.frameX = 0;
-        this.frameY = 0;
-        this.speed = 9;
-        this.gravity = 5;
-        this.health = 100;
-        this.moving = false;
-        this.jumping = false;
-        console.log('player1 2 created!');
+// White shurikens
+var shurikenArrayWhite = [];
+var shurikenWhite = /** @class */ (function () {
+    function shurikenWhite() {
+        this.x = canvas.width + Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.radius = 10;
+        this.speed = Math.random() * 5 + 10;
+        this.counted = false;
+        this.distance = 0;
     }
-    return player2Class;
+    shurikenWhite.prototype.update = function () {
+        this.x -= this.speed;
+        var dx = this.x - player1.x;
+        var dy = this.y - player1.y;
+        this.distance = Math.sqrt(dx * dx + dy * dy);
+    };
+    shurikenWhite.prototype.draw = function () {
+        ctx === null || ctx === void 0 ? void 0 : ctx.fillStyle = "white";
+        ctx === null || ctx === void 0 ? void 0 : ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx === null || ctx === void 0 ? void 0 : ctx.fill();
+        ctx === null || ctx === void 0 ? void 0 : ctx.closePath();
+        ctx === null || ctx === void 0 ? void 0 : ctx.stroke();
+    };
+    return shurikenWhite;
 }());
-// Shuriken class
-var shurikenClass = /** @class */ (function () {
-    function shurikenClass() {
-        this.x = 400;
-        this.y = 300;
-        this.width = 33;
-        this.height = 33;
-        this.frameX = 0;
-        this.frameY = 0;
-        this.speed = 9;
-        this.right = 9;
-        console.log('shuriken created!');
+function shurikenHandlerWhite() {
+    if (gameFrame % 10 == 0) {
+        shurikenArrayWhite.push(new shurikenWhite());
     }
-    return shurikenClass;
-}());
-// Creating players + shuriken
-var player1 = new player1Class;
-var player2 = new player2Class;
-var shuriken = new shurikenClass;
-function moveShuriken() {
-    shuriken.x += shuriken.right;
+    for (var i = 0; i < shurikenArrayWhite.length; i++) {
+        shurikenArrayWhite[i].update();
+        shurikenArrayWhite[i].draw();
+    }
+    for (var i = 0; i < shurikenArrayWhite.length; i++) {
+        if (shurikenArrayWhite[i].x < 0) {
+            shurikenArrayWhite.splice(i, 1);
+        }
+        if (shurikenArrayWhite[i].distance < shurikenArrayWhite[i].radius + player1.radius) {
+            scoreP1++;
+            if (!shurikenArrayWhite[i].counted) {
+                shurikenArrayWhite[i].counted = true;
+                shurikenArrayWhite.splice(i, 1);
+            }
+        }
+    }
 }
+// Black shurikens
+var shurikenArrayBlack = [];
+var shurikenBlack = /** @class */ (function () {
+    function shurikenBlack() {
+        this.x = canvas.width + Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.radius = 10;
+        this.speed = Math.random() * 5 + 10;
+        this.counted = false;
+        this.distance = 0;
+    }
+    shurikenBlack.prototype.update = function () {
+        this.x -= this.speed;
+        var dx = this.x - player2.x;
+        var dy = this.y - player2.y;
+        this.distance = Math.sqrt(dx * dx + dy * dy);
+    };
+    shurikenBlack.prototype.draw = function () {
+        ctx === null || ctx === void 0 ? void 0 : ctx.fillStyle = "black";
+        ctx === null || ctx === void 0 ? void 0 : ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx === null || ctx === void 0 ? void 0 : ctx.fill();
+        ctx === null || ctx === void 0 ? void 0 : ctx.closePath();
+        ctx === null || ctx === void 0 ? void 0 : ctx.stroke();
+    };
+    return shurikenBlack;
+}());
+function shurikenHandlerBlack() {
+    if (gameFrame % 10 == 0) {
+        shurikenArrayBlack.push(new shurikenBlack());
+    }
+    for (var i = 0; i < shurikenArrayBlack.length; i++) {
+        shurikenArrayBlack[i].update();
+        shurikenArrayBlack[i].draw();
+    }
+    for (var i = 0; i < shurikenArrayBlack.length; i++) {
+        if (shurikenArrayBlack[i].x < 0) {
+            shurikenArrayBlack.splice(i, 1);
+        }
+        if (shurikenArrayBlack[i].distance < shurikenArrayBlack[i].radius + player2.radius) {
+            scoreP2++;
+            if (!shurikenArrayBlack[i].counted) {
+                shurikenArrayBlack[i].counted = true;
+                shurikenArrayBlack.splice(i, 1);
+            }
+        }
+    }
+}
+var player1 = new playersClass;
+var player2 = new playersClass;
 // Adding sprites 
 var player1Sprite = new Image();
 player1Sprite.src = "docs/images/ninja_black.png";
 var player2Sprite = new Image();
 player2Sprite.src = "docs/images/ninja_white.png";
-var shurikenSprite = new Image;
-shurikenSprite.src = "docs/images/shuriken.png";
+// let shuriken1Sprite = new Image;
+// shuriken1Sprite.src = "docs/images/shuriken1.png";
 // Adding background
 var background = new Image();
 background.src = "docs/images/background.png";
@@ -81,22 +144,30 @@ function drawSprite1(img, sX, sY, sW, sH, dX, dY, dW, dH) {
 function drawSprite2(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
-function drawSprite3(img, sX, sY, sW, sH, dX, dY, dW, dH) {
-    ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
-}
-// Animates background, players and shurikens
 function animate() {
-    ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite1(player1Sprite, player1.width * player1.frameX, player1.height * player1.frameY, player1.width, player1.height, player1.x, player1.y, player1.width / 2, player1.height / 2);
-    drawSprite2(player2Sprite, player2.width * player2.frameX, player2.height * player2.frameY, player2.width, player2.height, player2.x, player2.y, player2.width / 2, player2.height / 2);
-    drawSprite3(shurikenSprite, shuriken.width * shuriken.frameX, shuriken.height * shuriken.frameY, shuriken.width, shuriken.height, shuriken.x, shuriken.y, shuriken.width / 2, shuriken.height / 2);
-    moveplayer1();
-    moveplayer2();
-    moveShuriken();
-    requestAnimationFrame(animate);
+    if (!win) {
+        ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        ctx === null || ctx === void 0 ? void 0 : ctx.fillText("points:" + scoreP1, 10, 20);
+        ctx === null || ctx === void 0 ? void 0 : ctx.fillText("points:" + scoreP2, 650, 20);
+        ctx === null || ctx === void 0 ? void 0 : ctx.font = '20px ariel';
+        drawSprite1(player1Sprite, player1.width * player1.frameX, player1.height * player1.frameY, player1.width, player1.height, player1.x, player1.y, player1.width / 2, player1.height / 2);
+        drawSprite2(player2Sprite, player2.width * player2.frameX, player2.height * player2.frameY, player2.width, player2.height, player2.x, player2.y, player2.width / 2, player2.height / 2);
+        moveplayer1();
+        moveplayer2();
+        requestAnimationFrame(animate);
+        shurikenHandlerWhite();
+        shurikenHandlerBlack();
+        gameFrame++;
+        checkWin();
+    }
 }
 animate();
+function setPlayerPositions() {
+    player1.x = 50;
+    player2.x = 650;
+}
+setPlayerPositions();
 // Adding keys
 window.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true;
@@ -117,22 +188,18 @@ function moveplayer1() {
         player1.y += player1.gravity;
     }
     if (keys[87] && player1.y > 30) {
-        console.log("player 1 up");
         player1.frameX = 0;
         jump1();
     }
     if (keys[83] && player1.y < canvas.height - player1.height + 320) {
-        console.log("player 1 down");
         player1.y += player1.speed;
         player1.frameX = 0;
     }
     if (keys[65] && player1.x > 20) {
-        console.log("player 1 left");
         player1.x -= player1.speed;
         player1.frameX = 2;
     }
     if (keys[68] && player1.x < canvas.width - player1.width + 60) {
-        console.log("player 1 right");
         player1.x += player1.speed;
         player1.frameX = 1;
     }
@@ -142,33 +209,34 @@ function moveplayer2() {
         player2.y += player2.gravity;
     }
     if (keys[38] && player2.y > 30) {
-        console.log("player 2 up");
         player2.frameX = 0;
         jump2();
     }
     if (keys[40] && player2.y < canvas.height - player2.height + 320) {
-        console.log("player 2 down");
         player2.y += player2.speed;
         player2.frameX = 0;
     }
     if (keys[37] && player2.x > 20) {
-        console.log("player 2 left");
         player2.x -= player2.speed;
         player2.frameX = 2;
     }
     if (keys[39] && player2.x < canvas.width - player2.width + 60) {
-        console.log("player 2 right");
         player2.x += player2.speed;
         player2.frameX = 1;
     }
 }
-// Players healthbar
-var delayInMilliseconds = 200; // 0.1 second delay
-setTimeout(function () {
-    var healthP1;
-    healthP1 = document.getElementById('p1-health');
-    healthP1.style.width = player1.health + "%";
-    var healthP2;
-    healthP2 = document.getElementById('p2-health');
-    healthP2.style.width = player2.health + "%";
-}, delayInMilliseconds);
+var delayInMilliseconds = 100; // Adds a 0.1 second delay untill game stops after a player wins
+function checkWin() {
+    if (scoreP1 >= 10) {
+        ctx === null || ctx === void 0 ? void 0 : ctx.fillText("Black ninja wins!", 10, 50);
+        setTimeout(function () {
+            win = true;
+        }, delayInMilliseconds);
+    }
+    if (scoreP2 >= 10) {
+        ctx === null || ctx === void 0 ? void 0 : ctx.fillText("White ninja wins!", 650, 50);
+        setTimeout(function () {
+            win = true;
+        }, delayInMilliseconds);
+    }
+}
